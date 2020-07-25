b07e78e7cc4aebc9cc6e237152a81de9957737d1
中村昌暉 <smith@nakamuramasaakiranoMacBook-Pro.local>
2020-07-25 18:48:30 +0900

    Created base-button component

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,7 +40,10 @@ export default {
   ** Plugins to load before mounting the App
   ** https://nuxtjs.org/guide/plugins
   */
-  plugins: ['~plugins/vform'],
+  plugins: [
+    '~plugins/vform',
+    '~components/_global'
+  ],
   /*
   ** Auto import components
   ** See https://nuxtjs.org/api/configuration-components
