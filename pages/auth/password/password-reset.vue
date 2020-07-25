625f98fe8ac735bc297ece55fd1d2fe69041e153
中村昌暉 <smith@nakamuramasaakiranoMacBook-Pro.local>
2020-07-25 17:58:40 +0900

    Reset Password methods updated

--- pages/auth/password/password-reset.vue
+++ pages/auth/password/password-reset.vue
@@ -5,10 +5,17 @@
                 Reset Password
             </h1>
             <form class="auth-form" @submit.prevent="submit">
+                <alert-success :form="form">
+                    {{ status }}
+                    <p>
+                        <nuxt-link to="/login">Proceed to login</nuxt-link>
+                    </p>
+                </alert-success>
                 <div class="form-group">
                     <input
                         type="text"
                         name="email"
+                        readonly
                         v-model="form.email"
                         class="form-control form-control-lg font-14 fw-300"
                         :class="{ 'is-invalid' :form.errors.has('email') }"
@@ -16,6 +23,29 @@
                     />
                     <has-error :form="form" field="email"></has-error>
                 </div>
+
+                <div class="form-group">
+                    <input
+                        type="password"
+                        name="password"
+                        v-model="form.password"
+                        class="form-control form-control-lg font-14 fw-300"
+                        :class="{ 'is-invalid' :form.errors.has('password') }"
+                        placeholder="New Password"
+                    />
+                    <has-error :form="form" field="password"></has-error>
+                </div>
+
+                <div class="form-group">
+                    <input
+                        type="password"
+                        name="password_confirmation"
+                        v-model="form.password_confirmation"
+                        class="form-control form-control-lg font-14 fw-300"
+                        placeholder="Confirm New Password"
+                    />
+                </div>
+
                 <div class="text-right">
                     <button
                         type="submit"
@@ -25,14 +55,9 @@
                         <span v-if="form.busy">
                             <i class="fas fa-spinner fa-spin"></i>
                         </span>
-                        Send Reset Link
+                        Reset Password
                     </button>
                 </div>
-                <p class="font-14 fw-400 text-center mt-4">
-                    <nuxt-link to="/login" class="color-blue">
-                        Back to login
-                    </nuxt-link>
-                </p>
             </form>
         </div>
     </section>
@@ -42,14 +67,28 @@
 export default {
     data(){
         return {
+            status: '',
             form: this.$vform({
-                email: ''
+                email: '',
+                password: '',
+                password_confirmation: '',
+                token: ''
             })
         }
     },
+    created(){
+        this.form.email = this.$route.query.email
+        this.form.token = this.$route.params.token
+    },
     methods: {
         submit(){
-
+            this.form.post('/password/reset')
+            .then(res => {
+                this.status = res.data.status;
+                this.form.reset();
+            }).catch(e => {
+                console.log(e)
+            })
         }
     }
 };
