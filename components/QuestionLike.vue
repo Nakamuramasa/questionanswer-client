716b8f05bbeda34e390ddb46fe13108263968481
中村昌暉 <smith@nakamuramasaakiranoMacBook-Pro.local>
2020-07-31 03:33:15 +0900

    Recreated Question like methods

--- components/QuestionLike.vue
+++ components/QuestionLike.vue
@@ -1,21 +1,16 @@
 <template>
     <li class="d-table w-100">
         <div class="stats-txt d-table-cell w-50">
-            <a href="#" @click.prevent="likeQuestion()" v-if="$auth.loggedIn">
-                <template v-if="current_user_likes">
+            <a href="#" @click.prevent="likeIt" v-if="$auth.loggedIn">
+                <template>
                     <span>
                         <i class="fas fa-heart fa-2x text-danger"></i>
-                    </span>
-                </template>
-                <template v-else>
-                    <span>
-                        <i class="fas fa-heart fa-2x"></i>
                     </span>
                 </template>
             </a>
         </div>
         <div class="stats-num d-table-cell w-50 text-right">
-            <a href="#">{{ total_likes }} Likes</a>
+            <a href="#">{{ count }} Likes</a>
         </div>
     </li>
 </template>
@@ -25,23 +20,29 @@ export default {
     props: ['question'],
     data() {
         return {
-            current_user_likes: false,
-            total_likes: 0
+            liked: false,
+            count: 0
         };
     },
-    methods: {
-        likeQuestion() {
-            this.$axios.post(`/questions/${this.question.id}/like`).then(res => {
-                this.current_user_likes = !this.current_user_likes;
-                this.total_likes = res.data.total;
-            });
-        },
-        checkIfCurrentUserLikes() {
-            this.$axios
-            .$get(`/questions/${this.question.id}/liked`)
-            .then(res => (this.current_user_likes = response.liked));
-        }
+    created(){
     },
-    created() {}
+    methods: {
+        likeIt(){
+            this.liked ? this.incr() : this.decr()
+            this.liked = !this.liked
+        },
+        incr(){
+            this.count++
+        },
+        decr(){
+            this.count--
+        },
+        likeQuestion(){
+            this.$axios.post(`/questions/${this.question.id}/like`)
+        },
+        checkIfCurrentUserLikes(){
+            this.$axios.$get(`/questions/${this.question.id}/liked`)
+        }
+    }
 };
 </script>
